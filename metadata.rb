9cc7d858a7dc742c1565a92ae918db152db4fee7
Arnold Krille <a.krille@b-c-s.de>
2013-10-08 22:43:59 +0200

    prepare metadata for a 0.1.1 release

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,16 @@ description      'Installs/Configures clonezillalive'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.1'
 
+provides         'clonezillalive::default'
+provides         'clonezillalive::server'
+
+attribute        'clonezilla/serverip',
+                 :display_name => 'Server IP',
+                 :description => 'Set a special ip for the clients to use when booting. For example if your server lives in two networks and clients should use the second interfaces ip-address. node[\'ipaddress\'] is used if this is unset.',
+                 :type => 'string',
+                 :required => 'optional',
+                 :default => nil
+
 supports         'debian', '>= 7.0'
 
 depends          'tftp'
